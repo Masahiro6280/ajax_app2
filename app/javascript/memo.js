19d09e763eb0212b199bb8d4b53aff15e82a3239
Masahiro6280 <purple08260826@gmail.com>
2021-09-13 03:00:23 +0900

    JSでレスポンスを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,17 +1,39 @@
+const buildHTML = (XHR) => {
+const item = XHR.response.post;
+      const html = `
+        <div class="post">
+          <div class="post-date">
+            投稿日時：${item.created_at}
+          </div>
+          <div class="post-content">
+            ${item.content}
+          </div>
+        </div>`;
+return html;
+};
+
+
+
 function post (){
   const submit = document.getElementById("submit");
-  console.log(submit)
   submit.addEventListener("click", (e) => {
    e.preventDefault();
     const form = document.getElementById("form");
-    console.log(form)
     const formData = new FormData(form);
-    console.log(formData)
     const XHR = new XMLHttpRequest();
-    console.log(XHR)
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
     XHR.send(formData);
+    XHR.onload = () => {
+      if (XHR.status != 200) {
+        alert(`Error ${XHR.status}: ${XHR.statusText}`);
+        return null;
+      };
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+        formText.value = "";
+    };
   });
  };
  
